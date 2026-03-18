ab37d0a9141020b12dcc4bc2c097aa1c3937f008
Алексей Горшков <aleksgor@gmail.com>
2026-03-18 15:18:43 +0300

    make input fields

--- src/main/java/ag/com/dbo/config/RestConfig.java
+++ src/main/java/ag/com/dbo/config/RestConfig.java
@@ -5,6 +5,9 @@ import org.springframework.aot.hint.annotation.RegisterReflectionForBinding;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
+import org.thymeleaf.spring6.SpringTemplateEngine;
+import org.thymeleaf.spring6.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.spring6.view.ThymeleafViewResolver;
 
 
 @Configuration
@@ -16,4 +19,21 @@ public class RestConfig {
         return new RestTemplate();
     }
 
+
+    @Bean
+    public ThymeleafViewResolver viewResolver() {
+        ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
+
+        SpringResourceTemplateResolver templateResolver = new SpringResourceTemplateResolver();
+        templateResolver.setPrefix("classpath:/templates");
+        templateResolver.setSuffix(".html");
+        // some other lines neglected...
+
+        SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+        templateEngine.setTemplateResolver(templateResolver);
+        // some other lines neglected...
+
+        viewResolver.setTemplateEngine(templateEngine);
+        return viewResolver;
+    }
 }
--- src/main/java/ag/com/dbo/controllers/EtlController.java
+++ src/main/java/ag/com/dbo/controllers/EtlController.java
@@ -3,10 +3,15 @@ package ag.com.dbo.controllers;
 import ag.com.dbo.models.Etl;
 import ag.com.dbo.repositories.EtlRepository;
 import ag.com.dbo.services.EtlService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
+@Slf4j
 @Controller
 public class EtlController {
 
@@ -21,5 +26,25 @@ public class EtlController {
         Iterable<Etl> etls = etlService.retrieveAll();
         model.addAttribute("list", etls);
         return "browser1";    }
+
+    @PostMapping("/etlForm")
+    public String saveEtl(
+            @ModelAttribute("etl") Etl model) {
+        log.info("saveEtl"+model);
+
+//        Iterable<Etl> etls = etlService.retrieveAll();
+//        model.addAttribute("list", etls);
+        return "browser1";
+    }
+
+
+    @GetMapping("/addEtlForm")
+    public String browser2( Model model) {
+        Etl etl = new Etl();
+        etl.setName("Testname");
+        model.addAttribute("etl",etl);
+
+        return "addEtlForm";
+    }
 
 }
